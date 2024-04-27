db304a7e2c820c6c49d1055494c910990d7efef3
JuleTrotzki <tj4062@mci4me.at>
2024-04-27 22:22:00 +0200

    Aufgabe62

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from my_functions import estimate_max_hr, build_person, build_experiment
 import json
-from my_classes import Person, Experiment
+from my_classes import Person, Experiment, Supervisor, Subject
 
 def main():
     experiment_name = input("Enter experiment name: ")
     date = input("Enter date of experiment (YYYY-MM-DD): ")
     supervisor_first_name = input("Enter supervisor's first name: ")
     supervisor_last_name = input("Enter supervisor's last name: ")
-    supervisor_sex = input("Enter supervisor's sex (male/female): ")
-    supervisor_age = int(input("Enter supervisor's age: "))
     subject_first_name = input("Enter subject's first name: ")
     subject_last_name = input("Enter subject's last name: ")
     subject_sex = input("Enter subject's sex (male/female): ")
-    subject_age = int(input("Enter subject's age: "))
+    subject_birthdate = (input("Enter subject's birthdate (YYYY-MM-DD): "))
 
-    supervisor = Person(supervisor_first_name, supervisor_last_name, supervisor_sex, supervisor_age)
-    subject = Person(subject_first_name, subject_last_name, subject_sex, subject_age)
+    supervisor = Supervisor(supervisor_first_name, supervisor_last_name)
+    subject = Subject(subject_first_name, subject_last_name, subject_sex, subject_birthdate)
 
-    experiment = Experiment(experiment_name, date, supervisor.to_dict(), subject.to_dict())
+    experiment = Experiment(experiment_name, date, supervisor, subject)
 
     print("Experiment details:")
     print(experiment.__dict__)
--- my_classes.py
+++ my_classes.py
@@ -1,37 +1,64 @@
 import json 
-from my_functions import estimate_max_hr
-from my_functions import build_person
+from datetime import datetime
 
-
+# ------------Personen-Klasse ------------------------------------------------------------------------------
 class Person:
-    def __init__(self, first_name, last_name, sex, age):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.sex = sex
-        self.age = age
-        self.max_hr = self.estimate_max_hr()
-        
-    def estimate_max_hr(self):
-        return estimate_max_hr(self.age, self.sex)
-    
+
     def to_dict(self):
         print(self.__dict__) 
         return {
             "first_name": self.first_name,
+            "last_name": self.last_name}            
+          
+#----------Subject-Klasse-----------------------------------------------------------------------------------            
+class Subject(Person):
+    def __init__(self, first_name, last_name, sex, birthdate):
+        super().__init__(first_name, last_name)
+        self.sex = sex 
+        self._birthdate = birthdate
+        self._age = self.calculate_age()
+        self.max_hr_bpm = self.calculate_max_hr()
+        
+    def calculate_max_hr(self):
+        age = self.calculate_age()
+        if self.sex == "male":
+            max_hr_bpm = 223 - 0.9 * age
+        elif self.sex == "female":
+            max_hr_bpm = 226 - 1.0 * age
+       
+        return int(max_hr_bpm)
+
+    def calculate_age(self):
+        today = datetime.today()
+        birthdate = datetime.strptime(self._birthdate, "%Y-%m-%d")
+        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        return age
+        
+    def to_dict(self):
+        return {
+            "first_name": self.first_name,
             "last_name": self.last_name,
             "sex": self.sex,
-            "age": self.age
-        }
+            "age": self._age,
+            "max_hr_bpm": self.max_hr_bpm}
+        
+#-----------Supervisor-Klasse------------------------------------------------------------------------------
+class Supervisor(Person):
 
-    def save(self, filename):
-        with open(filename, "w") as f:
-            json.dump(self.to_dict(), f, indent=4)
+    def to_dict(self):
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name}
 
 
+#------------Experiment-Klasse----------------------------------------------------------------------------
 class Experiment:
     def __init__(self, experiment_name, date, supervisor, subject):
         self.experiment_name = experiment_name
-        self.date = date
+        self.date = date 
         self.supervisor = supervisor
         self.subject = subject
         
@@ -39,16 +66,11 @@ class Experiment:
         return {
             "experiment_name": self.experiment_name,
             "date": self.date,
-            "supervisor": self.supervisor,
-            "subject": self.subject
-        }
+            "supervisor": self.supervisor.to_dict(),
+            "subject": self.subject.to_dict()
+            }
 
     def save(self, filename):
         with open(filename, "w") as f:
             json.dump(self.to_dict(), f, indent=4)
-         
-            
-
-
-                   
-            
+                           
