5038df2d0cf30c729eff0219b334541cb7f02c7b
JuleTrotzki <tj4062@mci4me.at>
2024-05-05 17:28:04 +0200

    Aufgabe8

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 from my_classes import Subject, Supervisor, Experiment
 
+
 def main():
     
     # Beispiel-Subject erstellen und über die API senden
-    subject = Subject("Julian", "Maier", "male", "1994-07-16", "julian@mail.at")
+    subject = Subject("Matthias", "Hauck", "male", "1994-07-16", "Matze@mail.at")
     subject.put()
 
     # Beispiel-Email aktualisieren und über die API senden
-    subject.email = "juls.maier@mail.at"
+    subject.email = "mathhias.hauck@mail.at"
     subject.update_email()
 
     # Beispiel-Experiment erstellen und speichern
     supervisor = Supervisor("Holger", "Schwarz")
     experiment = Experiment("Example Experiment", "2024-05-05", supervisor, subject)
-    experiment.save("example_experiment.json")
+    experiment.save("data.json")
+    
 
 if __name__ == "__main__":
     main()
+    
+
